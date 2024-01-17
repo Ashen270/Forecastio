8daeb154a04f470b9edd8213e1b04e452a9883ed
Ashen Shamilka Wickramasinghe <ashenshamilka270@gmail.com>
2024-01-17 23:20:56 +0530

    4

--- forcastio/src/componet/forecastioApp/Forecastio.jsx
+++ forcastio/src/componet/forecastioApp/Forecastio.jsx
@@ -18,16 +18,39 @@ return (
 
         <div className="top-bar">
          <input type="text" className="cityInput"  placeholder='Search'/>
-            <div className="searchicon">
+            <div className="search-icon">
               <img src ={search_icon} alt=''/>
 
             </div>
         </div>
+      <div className="weatherimage">
+          <img src ={cloud_icon} alt=""/>
+      </div>
+      <div className="weathertemp">24°c</div>
+      <div className="weatherlocation">Colombo</div>
+      <div className="datacontainer">
+         <div className="element">
+
+           <img src={humidity_icon} alt="" className="icon" />
+           <div className="data">
+            <div className="humiditypercentage">64%</div>
+            <div className="text">Humidity</div>
+           </div>
+         </div>
+         <div className="element">
+           <img src={wind_icon} alt="" className="icon" />
+           <div className="data">
+            <div className="humiditypercentage">18 Km/h</div>
+            <div className="text">Wind Speed</div>
+           </div>
+         </div>
+      </div>
 
 
+             
      </div>
 
-)
+);
 
 
 }
