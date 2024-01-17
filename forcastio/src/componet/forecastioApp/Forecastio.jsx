2fca9b48a51aa0ce6c961be7bcb4bdb55ecaa11e
Ashen Shamilka Wickramasinghe <ashenshamilka270@gmail.com>
2024-01-18 01:19:28 +0530

    Update Forecastio.jsx

--- forcastio/src/componet/forecastioApp/Forecastio.jsx
+++ forcastio/src/componet/forecastioApp/Forecastio.jsx
@@ -12,13 +12,35 @@ import wind_icon from '../assests/wind.png';
 
 const Forecastio = () => {
 
+   let api_key = "0125b1043dbd646bad4b9a2fe954022a";
+
+   const search = async () => {
+         const elemet =document.getElementsByClassName("cityInput")
+         if(elemet[0].value==="")  
+   
+        {
+            return 0;
+
+        }
+
+        let url =`https://api.openweathermap.org/data/2.5/weather?q=${elemet[0].value}&appid=${api_key}`;
+        
+        let response = await fetch(url);
+        let data = await response.json();
+        const humidity=document.getElementsByClassName("humiditypercentage");
+        const wind = document.getElementsByClassName("windrate");
+        const temprature = document.getElementsByClassName("weathertemp");
+        const location = document.getElementsByClassName("weatherlocation");
+
+   }
+
 return (
 
      <div className='container'>
 
         <div className="top-bar">
          <input type="text" className="cityInput"  placeholder='Search'/>
-            <div className="search-icon">
+            <div className="search-icon" onClick={() =>{search()}}>
               <img src ={search_icon} alt=''/>
 
             </div>
@@ -33,7 +55,7 @@ return (
 
            <img src={humidity_icon} alt="" className="icon" />
            <div className="data">
-            <div className="humiditypercentage">64%</div>
+            <div className="windrate">64%</div>
             <div className="text">Humidity</div>
            </div>
          </div>
